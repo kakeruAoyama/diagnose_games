20b2192d87387987e69849a608a594abc0dc26e5
kakeruAoyama <64327383+kakeruAoyama@users.noreply.github.com>
2022-06-24 08:20:55 +0900

    Update tag.rb

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,4 @@
 class Tag < ApplicationRecord
   has_many :tag_games_rerationships, dependent: :destroy
-  has_many :tweets, through: :tag_games_rerationships, dependent: :destroy
+  has_many :games, through: :tag_games_rerationships, dependent: :destroy
 end
